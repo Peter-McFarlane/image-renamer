9cb7819e0c5d10435edebe58d98cb317ad8c52d0
Peter McFarlane <71842465+Peter-McFarlane@users.noreply.github.com>
2023-05-28 16:46:46 -0400

    tweaked program

--- formatter.py
+++ formatter.py
@@ -1,11 +1,9 @@
-# format filename from EXIF data
 # YYYY-MM-DD  HH'mm'SS
 
 # INSTRUCTIONS:
 # 1. place this file into the directory where your photos are located
-# 2. copy the directory and paste it into line 19 of this program
-# 3. run the program
-# 4. sometimes the program will get stuck on a file;
+# 2. run the program
+# 3. sometimes the program will get stuck on a file;
 #    if you get a "no such file or directory error",
 #    simply run the program again and it should unstuck itself
 
@@ -16,13 +14,15 @@ from exif import Image
 burst_i = 0 # set burst index to 0
 last = ''
 
-root_dir = '.' # <-- input root directory here
+root_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir += '/'
+
 for path in pathlib.Path(root_dir).iterdir():
     info = path.stat()
 
     # skip unsupported filetypes
     file_name, file_extension = os.path.splitext(path)
-    supported = ['.jpg', '.tif', '.wav', '.png', '.webp']
+    supported = ['.jpg', '.JPG', '.tif', '.wav', '.png', '.webp']
     if file_extension not in supported:
         continue
 
@@ -50,9 +50,11 @@ for path in pathlib.Path(root_dir).iterdir():
     if date_and_time[0:20] == last[0:20]:
         burst_i = burst_i + 1
         new_filename = date_and_time + "__" + str(burst_i) + file_extension
+        new_filename = os.path.join(os.path.dirname(root_dir), new_filename)
         os.rename(path, new_filename)
     else:
         new_filename = date_and_time + file_extension
+        new_filename = os.path.join(os.path.dirname(root_dir), new_filename)
         os.rename(path, new_filename)
         last = date_and_time # keep track of the last date/time
         burst_i = 0 # reset the burst index
