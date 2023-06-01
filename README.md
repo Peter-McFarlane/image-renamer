48b567b7bc3c51601d788bb780a18453a22eb4a7
Peter McFarlane <71842465+Peter-McFarlane@users.noreply.github.com>
2023-06-01 18:52:03 -0400

    implemented GUI

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # image-renamer
-Rename a JPEG image to the date and time it was taken from its exif data
+
+## What this program does and who it’s for
+
+This tool renames media files to the date and time at which they were created. 
+
+![before and after](./images/before_and_after.png)
+
+If the image is a JPEG, it gets the timestamp from its Exif data. For PNGs, video files, and audio files, it gets the timestamp from either its created date or its last modified date (whichever is earliest).
+
+This is a handy tool for properly sorting images chronologically, especially if you’re dealing with images from a DSLR that uses unhelpful file naming conventions like “IMG_0037.JPG” (multiple images can have the same name if they’re taken from different SD cards, making it obnoxious to merge all of these images into the same file).
+
+This tool is also an effective way to chronologically organize photos from different sources. I originally developed this tool to organize family photos from numerous old hard drives spanning over a decade.
+
+## What’s the deal with all these dates?
+
+The created date refers to the date the file was created on the file system on which it is currently located. The modified date refers to the date the file was last written to on the system. These timestamps are sometimes, but not necessarily the time at which the file was initially created. For example, this image of me taken on Christmas morning of 2006 does not show the correct timestamps here:
+
+![xmas](./images/xmas.png)
+
+For this reason, this tool works best with JPEGs, since JPEGs contain the date and time the photo was originally taken embedded in its Exif data. When we take a look at the Exif data for the above image, we can see the correct date and time:
+
+![exif example](./images/exif_example.png)
+
+For non-JPEG files, be sure to check that the created and/or modified times seem correct before feeding them into the program.
+
+## Sometimes Exif data is wrong
+
+This is an photo of me swimming in a lake in broad daylight taken sometime in the summer of 2007:
+
+![lake](./images/lake.jpg)
+
+However, the Exif data shows that the photo was taken shortly after midnight on January 10th?
+
+![wrong exif](./images/wrong_exif.png)
+
+This is because the date and time was not properly set on the camera this picture was taken on, probably because whoever set up the camera simply breezed through the setup process and kept the date set to its default value of January 1st, 2007 at 12:00:00.
+
+Be sure to do a cursory check to see that the Exif data makes sense, particularly if you’re trying to organize older photos where the date and time may not have been properly configured.
+
+## How to check the Exif data
+
+On Mac, open the JPEG image in Preview and press cmd+I. Then, navigate to the “More Info” tab and select “Exif”.
+
+On Windows, right-ckick the desired JPEG. Then, select “Properties”. Then, select the “Details” tab.
---
+++ image-renamer.py
@@ -0,0 +1,192 @@
+import os
+import stat
+import pathlib
+import webbrowser
+import tkinter as tk
+from exif import Image
+from tkinter import ttk
+from pathlib import Path
+from natsort import natsorted
+from datetime import datetime
+
+
+# ======= FUNCTIONS =======
+
+# redirect to README.md
+def callback(url):
+    webbrowser.open_new_tab(url)
+
+# clear GUI messages
+def cleartext():
+    error.set(" ")
+    status_label.config(text=" ")
+    num_renamed_label.config(text=" ")
+    num_skipped_label.config(text=" ")
+    root.update()
+
+# handle path from entry
+def get_path():
+    pathname = entry.get()
+    if os.path.exists(pathname):
+        if (pathname[len(pathname) - 1] != '/'):
+            pathname += '/'
+        cleartext()
+        run(pathname)
+    else:
+        error.set("Error: that is not a valid pathname")
+
+# convert unix timestamp to proper datetime format
+def unix_to_datetime(unix):
+    return datetime.utcfromtimestamp(unix).strftime("%Y-%m-%d [%H∶%M∶%S]")
+
+# rename files that do not have exif data according to created/modified time
+def rename_non_jpeg(path):
+    created = path.stat().st_ctime
+    modified = path.stat().st_mtime
+
+    if created <= modified:
+        date_and_time = unix_to_datetime(created)
+    else:
+        date_and_time = unix_to_datetime(modified)
+
+    return date_and_time
+
+# rename the files
+def run(root_dir):
+    button.config(state="disabled")
+    num_renamed = 0
+    num_skipped = 0
+    burst_i = 0 # keep burst index at 0 by default
+    last = '' # previous timestamp
+
+    # sort images by name so that burst photos don't get overwritten
+    image_list = Path(root_dir).glob('*')
+    image_list = natsorted(image_list, key=str)
+
+    for path in image_list:
+        file_name, file_extension = os.path.splitext(path)
+
+        # supported file extensions
+        jpeg_extns = ['.jpg', '.jpeg']
+        other_media_extns = ['.mpeg', '.wmv', '.avi', '.mov', 
+                             '.mkv', '.png', '.gif', '.tiff']
+
+        # rename non-jpeg files
+        if file_extension.lower() in other_media_extns:
+
+            # update counter and status
+            num_renamed += 1
+            status_label.config(text="Renaming: " + str(file_name), fg="purple")
+            root.update()
+
+            # rename non-jpeg file
+            date_and_time = rename_non_jpeg(path)
+            new_filename = date_and_time + file_extension
+            new_filename = os.path.join(os.path.dirname(root_dir), new_filename)
+            os.rename(path, new_filename)
+        
+        # rename jpeg images
+        elif file_extension.lower() in jpeg_extns:
+
+            # modify date and time format to my liking
+            # YYYY-mm-DD [HH∶MM∶SS]
+            img = Image(path)
+            if img.has_exif and img.get("datetime_original") != "0000:00:00 00:00:00":
+
+                # update counter and status
+                num_renamed += 1
+                status_label.config(text="Renaming: " + str(file_name), fg="purple")
+                root.update()
+
+                # format date and time
+                date_list = list(img.get("datetime_original"))
+                date_list[4] = '-'
+                date_list[7] = '-'
+                date_list[13] = "∶" # NOTE: this is not a colon, as they are not allowed to be used in
+                date_list[16] = "∶" # filenames on Mac. Rather, this is a ratio symbol (U+2236)
+                temp1 = date_list[0:11]
+                temp1.append("[")
+                temp2 = date_list[11:19]
+                temp2.append("]")
+                date_list = temp1 + temp2
+                date_and_time = "".join(date_list)
+            else:
+                # skip if there's no/bad exif data and update counter and status
+                num_skipped += 1
+                status_label.config(text="Skipping: " + str(file_name), fg="purple")
+                root.update()
+                continue
+
+            # if two or more images have the exact same timestamp, we need to append
+            # the burst index to the end so that photos don't get overwritten
+            if date_and_time[0:20] == last[0:20]:
+                burst_i = burst_i + 1
+                new_filename = date_and_time + "~" + str(burst_i) + file_extension
+                new_filename = os.path.join(os.path.dirname(root_dir), new_filename)
+                os.rename(path, new_filename)
+            else:
+                new_filename = date_and_time + file_extension
+                new_filename = os.path.join(os.path.dirname(root_dir), new_filename)
+                os.rename(path, new_filename)
+                last = date_and_time # keep track of the last date/time
+                burst_i = 0 # reset the burst index
+        
+        # ignore all other file extensions
+        else:
+            continue
+
+    status_label.config(text="Done!", fg="green")
+    num_renamed_label.config(text="Renamed " + str(num_renamed) + " files", fg="purple")
+    num_skipped_label.config(text="Skipped " + str(num_skipped) + " files", fg="purple")
+    button.config(state="normal")
+
+
+# ======= GUI =======
+
+root = tk.Tk()
+root.title("Image Renamer")
+root.geometry("700x500")
+
+# Readme label
+readme_label = tk.Label(root, text="❗️ Please read the README before running this program ❗️", font=("TkDefaultFont"))
+readme_label.pack(pady=10)
+
+# Readme link
+readme_link = tk.Label(root, text="Click here to read the README.md", font=("TkDefaultFont"), fg="blue", cursor="hand2")
+readme_link.pack(pady=10)
+readme_link.bind("<Button-1>", lambda e:
+callback("https://github.com/Peter-McFarlane/image-renamer/blob/main/README.md"))
+
+# Seperator
+separator = ttk.Separator(root, orient='horizontal')
+separator.pack(fill='x', pady=10)
+
+# Path label
+path_label = tk.Label(root, text="Enter path where images are located:", font=("TkDefaultFont"), anchor="w")
+path_label.pack(pady=10)
+
+# Path entry
+entry = tk.Entry(root, width=50)
+entry.pack(pady=10)
+
+# Error label
+error = tk.StringVar()
+error_label = tk.Label(root, textvariable=error, font=("TkFixedFont"), fg="red")
+error_label.pack(pady=10)
+
+# Run button
+button = tk.Button(root, text="  Run it  ", font=("TkDefaultFont"), command=get_path, cursor="hand2")
+button.pack(pady=10)
+
+# Status label
+status_label = tk.Label(root, text=" ", font=("TkFixedFont"), fg="purple")
+status_label.pack(pady=10)
+
+# Results label
+num_renamed_label = tk.Label(root, text=" ", font=("TkFixedFont"), fg="purple", anchor="w")
+num_skipped_label = tk.Label(root, text=" ", font=("TkFixedFont"), fg="purple", anchor="w")
+num_renamed_label.pack(pady=5)
+num_skipped_label.pack(pady=5)
+
+# Run the GUI
+root.mainloop()
